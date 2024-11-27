7b315c2f9b4854252756d78e0582e315254dc337
Mahendra <mahendrakondadasu@gmail.com>
2024-11-27 14:27:13 +0530

    feat: Add support for custom delimiters in string calculator and implement corresponding tests.

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,14 +2,25 @@ function add(string) {
     if (string === "") {
         return 0;
     }
-    string = string.replace("\n", ",");
-    const numbers = string.split(",").map((number) => parseInt(number));
+    const stringAfterReplacingNewLine = string.replaceAll("\n", ",");
+    const delimiter = getDelimeter(string);
+    const stringAfterReplacingDelimeter = stringAfterReplacingNewLine.replaceAll(delimiter, ",");
+    const numbers = stringAfterReplacingDelimeter.split(",").map((number) => parseInt(number)).filter((number) => !isNaN(number));
     if (numbers.length === 1) {
         return numbers[0];
     }
     return numbers.reduce((acc, number) => acc + number);
 }
 
+function getDelimeter(string) {
+    const delimiterStartIndex = string.indexOf("//");
+    if (delimiterStartIndex !== -1) {
+        return string[delimiterStartIndex + 2];
+    }
+    return null;
+}
+
 export {
-    add
+    add,
+    getDelimeter
 };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,4 +1,4 @@
-import { add } from './stringCalculator.js';
+import { add, getDelimeter } from './stringCalculator.js';
 
 describe('add', () => {
     it('should return 0 when given an empty string', () => {
@@ -15,5 +15,23 @@ describe('add', () => {
 
     it('should return 6 when given 1\n2,3', () => {
         expect(add("1\n2,3")).toBe(6);
+    });
+
+    it('should return 6 when given //;\n1;2;3', () => {
+        expect(add("//;\n1;2,3")).toBe(6);
+    });
+
+    it('should return 6 when given //a\n1a2a3', () => {
+        expect(add("//a\n1a2a3")).toBe(6);
+    });
+});
+
+describe('getDelimeter', () => {
+    it('should return ; when given //;\n1;2;3', () => {
+        expect(getDelimeter("//;\n1;2,3")).toBe(";");
+    });
+
+    it('should return a when given //a\n1a2a3', () => {
+        expect(getDelimeter("//a\n1a2a3")).toBe("a");
     });
 });

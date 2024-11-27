d24a80db5d377d7b453ebc1cbc52d5da6711c603
Mahendra <mahendrakondadasu@gmail.com>
2024-11-27 13:46:22 +0530

    feat: Implement addition for single number input in string calculator.

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,7 @@ function add(string) {
     if (string === "") {
         return 0;
     }
+    return parseInt(string);
 }
 
 export {
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('add', () => {
     it('should return 0 when given an empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    it('should return 1 when given "1"', () => {
+        expect(add("1")).toBe(1);
+    });
 });
